b7503a48c8252712d14701fae2d20d5fc7fbd456
LWF0824 <985579124@qq.com>
2021-12-04 09:53:39 +0800

    [发现和标签页面]

---
+++ src/components/Discover.vue
@@ -0,0 +1,113 @@
+<template>
+    <el-row>
+        <el-col :span="16">
+            <div class="grid-content bg-white">
+                <div id="tag-table">
+                    <el-table :data="tableData" stripe style="width: 100%">
+                        <el-table-column prop="tag_name" label="推荐标签" width="400" align="center">
+
+                        </el-table-column>
+                        <el-table-column prop="article_num" label="活跃度" width="400" align="center">
+
+                        </el-table-column>
+                    </el-table>
+                </div>
+            </div>
+        </el-col>
+        <el-col :span="8">
+            <div class="grid-content bg-white">
+                <div id="pic">
+                    <img src="../../src/assets/discover_pic1.png" width="350" height="350">
+                    <img src="../../src/assets/discover_pic2.png" width="300">
+                </div>
+            </div>
+        </el-col>
+    </el-row>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        tableData: [{
+          tag_name: '#原神',
+          article_num: '1234'
+        }, {
+          tag_name: '#明日方舟',
+          article_num: '156'
+        }, {
+          tag_name: '#咒术回战',
+          article_num: '265'
+        }, {
+          tag_name: '#鬼灭之刃',
+          article_num: '123'
+        }, {
+          tag_name: '#原神',
+          article_num: '1234'
+        }, {
+          tag_name: '#原神',
+          article_num: '1234'
+        }, {
+          tag_name: '#原神',
+          article_num: '1234'
+        }, {
+          tag_name: '#原神',
+          article_num: '1234'
+        }, {
+          tag_name: '#原神',
+          article_num: '1234'
+        }, {
+          tag_name: '#原神',
+          article_num: '1234'
+        }, {
+          tag_name: '#原神',
+          article_num: '1234'
+        }, {
+          tag_name: '#原神',
+          article_num: '1234'
+        }, {
+          tag_name: '#原神',
+          article_num: '1234'
+        }, {
+          tag_name: '#原神',
+          article_num: '1234'
+        }, {
+          tag_name: '#原神',
+          article_num: '1234'
+        }, {
+          tag_name: '#原神',
+          article_num: '1234'
+        }, {
+          tag_name: '#原神',
+          article_num: '1234'
+        }]
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .el-row {
+    margin-bottom: 20px;
+  }
+  .el-col {
+    border-radius: 4px;
+  }
+  .bg-white {
+    background: #ffffff;
+  }
+  .grid-content {
+    border-radius: 4px;
+    min-height: 36px;
+  }
+  .row-bg {
+    padding: 10px 0;
+    background-color: #f9fafc;
+  }
+  #tag-table {
+      float: right;
+  }
+  #pic{
+      position: fixed;
+  }
+</style>
---
+++ src/components/Tag.vue
@@ -0,0 +1,60 @@
+<template>
+    <el-row>
+        <el-col :span="24">
+            <div class="grid-content bg-purple-dark">
+                <div class="tag-name">
+                    <img src="../../src/assets/tag.png" width="30px" id="tag-pic">
+                    <span id="tag">{{tag_name}}</span>
+                    <span id="article-num">{{article_num}}参与</span>
+                </div>
+                <el-divider></el-divider>
+                <el-button id="join">参与话题</el-button>
+            </div>
+        </el-col>
+    </el-row>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                tag_name: "原神",
+                article_num: 1234
+            }
+        }
+    }
+
+</script>
+
+<style>
+  .el-row {
+    margin-bottom: 20px;
+  }
+  .el-col {
+    border-radius: 4px;
+  }
+  .bg-purple-dark {
+    background: #99a9bf;
+  }
+  .grid-content {
+    border-radius: 4px;
+    min-height: 36px;
+  }
+  .row-bg {
+    padding: 10px 0;
+    background-color: #f9fafc;
+  }
+  #tag-name {
+      vertical-align:middle;
+  }
+  #tag {
+      font-size: 26px;
+  }
+  #tag-pic {
+      padding-top: 10px;
+  }
+  #join {
+      background-color: #D55245;
+      color: rgb(255, 255, 255);
+  }
+</style>
