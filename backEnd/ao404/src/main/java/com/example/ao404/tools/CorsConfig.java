222415a1a7fb45b7bdc544899b4051bd00330b91
starryCcchj <1640448282@qq.com>
2021-12-10 10:24:17 +0800

    配置跨域

---
+++ backEnd/ao404/src/main/java/com/example/ao404/tools/CorsConfig.java
@@ -0,0 +1,22 @@
+package com.example.ao404.tools;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        System.out.println("----------------------");
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowCredentials(true)
+                .allowedMethods("GET", "POST", "DELETE", "PUT")
+                .allowedHeaders("*")
+                .maxAge(3600);
+    }
+
+
+}
